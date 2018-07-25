cd46a74c6018fda508de29c265a698993f16962f
Tobsi1234 <Tobias@tobias-mbp-2.speedport.ip>
2018-07-25 21:52:17 +0200

    Added mongodb_uri

--- repository.js
+++ repository.js
@@ -1,6 +1,6 @@
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test2');
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
